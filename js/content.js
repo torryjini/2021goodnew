b44810c8737d3d00e8e8d4a418a215f6278c7eb8
torryjini <torryjini@users.noreply.github.com>
2021-12-21 14:30:10 +0900

    Add files via upload

--- js/content.js
+++ js/content.js
@@ -19,16 +19,16 @@ function btnHandler(){
         content.innerHTML = `<h1>초대영상 준비 중입니다...</h1>`
         content.classList.remove("youtube")
     } else if(menu1.checked) {
-        content.innerHTML = `<h1>시안목장</h1><iframe src="https://www.youtube.com/embed/th_OC6M6B3c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        content.innerHTML = `<iframe src="https://www.youtube.com/embed/th_OC6M6B3c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
         content.classList.add("youtube")
     } else if(menu2.checked) {
-        content.innerHTML = `<h1>부키드논목장</h1><img class="poster" src="img/부키드논.jpg" alt="부키드논목장 포스터">`
+        content.innerHTML = `<img class="poster" src="img/부키드논.jpg" alt="부키드논목장 포스터">`
         content.classList.remove("youtube")
     } else if(menu3.checked) {
-        content.innerHTML = `<h1>쌀라띠까목장</h1><img class="poster" src="img/쌀라띠까.png" alt="쌀라띠까목장 포스터">`
+        content.innerHTML = `<img class="poster" src="img/쌀라띠까.png" alt="쌀라띠까목장 포스터">`
         content.classList.remove("youtube")
     } else if(menu4.checked) {
-        content.innerHTML = `<h1>약속의동산</h1><iframe src="https://www.youtube.com/embed/vC8KU-7kb0o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        content.innerHTML = `<iframe src="https://www.youtube.com/embed/vC8KU-7kb0o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
         content.classList.add("youtube")
     } else if(menu5.checked) {
         content.innerHTML = `<h1>동고비목장 준비 중입니다...</h1>`
@@ -46,7 +46,7 @@ function btnHandler(){
         content.innerHTML = `<h1>JG목장 준비 중입니다...</h1>`
         content.classList.remove("youtube")
     } else if(menu10.checked) {
-        content.innerHTML = `<h1>유치부</h1><iframe src="https://www.youtube.com/embed/GGAS-Z1OVNw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        content.innerHTML = `<iframe src="https://www.youtube.com/embed/GGAS-Z1OVNw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
         content.classList.add("youtube")
     } else if(menu11.checked) {
         content.innerHTML = `<h1>유초등부 준비 중입니다...</h1>`
